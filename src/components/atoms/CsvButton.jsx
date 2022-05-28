7a5de1c301e741671c66552740a08c808f756cd2
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-05-28 20:59:10 +0900

    リファクタ

--- src/components/atoms/CsvButton.jsx
+++ src/components/atoms/CsvButton.jsx
@@ -1,42 +1,46 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import { CSVLink, CSVDownload } from "react-csv";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { CSVLink } from "react-csv";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-	  '& > *': {
-		margin: theme.spacing(1),
-	  },
-	},
-	button: {
-		backgroundColor: "white",
-		marginTop: "30px",
-		marginBottom: "30px",
-		height: "50px",
-		width: "200px",
-		fontWeight: "bold",
-		fontSize: "15px",
-	}
-  }));
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  button: {
+    backgroundColor: "white",
+    marginTop: "30px",
+    marginBottom: "30px",
+    height: "50px",
+    width: "200px",
+    fontWeight: "bold",
+    fontSize: "15px",
+  },
+}));
 
 const headers = [
-	{ label: "書籍名", key: "title" },
-	{ label: "著者名", key: "author" },
-	{ label: "出版社", key: "publisher" },
-	{ label: "isbn", key: "isbn" },
-	{ label: "値段", key: "price" },
+  { label: "書籍名", key: "title" },
+  { label: "著者名", key: "author" },
+  { label: "出版社", key: "publisher" },
+  { label: "isbn", key: "isbn" },
+  { label: "値段", key: "price" },
 ];
 
-export const CsvButton = (props) => {
-	const {data} = props;
-	const classes = useStyles();
+/**
+ * CSV出力をするボタン
+ */
+export const CsvButton = ({ data }) => {
+  const classes = useStyles();
 
-	return (
-			<div className={classes.root}>
-				<CSVLink data={data} headers={headers}>
-					<Button variant="outlined" className={classes.button} >CSV化する</Button>
-				</CSVLink>
-			</div>
-	)
-}
+  return (
+    <div className={classes.root}>
+      <CSVLink data={data} headers={headers}>
+        <Button variant="outlined" className={classes.button}>
+          CSV化する
+        </Button>
+      </CSVLink>
+    </div>
+  );
+};
--- src/components/molecules/Search.jsx
+++ src/components/molecules/Search.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
@@ -12,49 +12,59 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   text: {
-	  backgroundColor: "white"
-  }
+    backgroundColor: "white",
+  },
 }));
 
-export const Search = (props) => {
-  const { isbn, setIsbn, data, setData } = props;
+/**
+ * 検索コンポーネント
+ */
+export const Search = ({ isbn, setIsbn, data, setData }) => {
   const [id, setId] = useState(1);
   const classes = useStyles();
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    onClickButton()
-  }
+    event.preventDefault();
+    onClickButton();
+  };
 
   const onClickButton = () => {
-	// -　があるISBNコードでも対応する 正規表現にすると全て置換できる
-	const replacedIsbn = isbn.replace(/-/g, '')
-	// 楽天書籍検索API
-  axios.get(`https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&isbn=${replacedIsbn}&applicationId=1000015805478906388`)
-	.then((res) => {
-		const bookData = res.data.Items[0].Item;
-		setId(id+1)
-		setData([
-			{
-				id: id,
-				title: bookData.title,
-				author: bookData.author,
-				publisher: bookData.publisherName,
-				isbn: bookData.isbn,
-				price: bookData.itemPrice
-			},
-			...data,
-		])
-	})
-	.catch((error) => {
-		console.log(error)
-	});
-  setIsbn("")
+    // -　があるISBNコードでも対応する 正規表現にすると全て置換できる
+    const replacedIsbn = isbn.replace(/-/g, "");
+    // 楽天書籍検索API
+    axios
+      .get(
+        `https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&isbn=${replacedIsbn}&applicationId=1000015805478906388`
+      )
+      .then((res) => {
+        const bookData = res.data.Items[0].Item;
+        setId(id + 1);
+        setData([
+          {
+            id: id,
+            title: bookData.title,
+            author: bookData.author,
+            publisher: bookData.publisherName,
+            isbn: bookData.isbn,
+            price: bookData.itemPrice,
+          },
+          ...data,
+        ]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setIsbn("");
   };
 
   return (
     <>
-      <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
+      >
         <TextField
           id="outlined-basic"
           label="isbnコード"
